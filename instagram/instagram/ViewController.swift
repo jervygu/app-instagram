ca76a20da1657d3c8c5a5a16e2cbed18b159f84b
Jeff Umandap <jeffumandap@Jeffs-MacBook-Pro.local>
2021-04-06 16:55:26 +0800

    Creation of Starter project.

---
+++ instagram/instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instagram
+//
+//  Created by Jeff Umandap on 4/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
